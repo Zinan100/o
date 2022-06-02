03bdd1715b564696ca556d02f1b3d15747d7619a
ZINAN <97530806+Zinan100@users.noreply.github.com>
2022-06-02 21:08:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Zinan100/srannan.git /Ajax
+  git clone https://github.com/Zinan100/srannan.git /sra
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Ajax
+  git clone $UPSTREAM_REPO /sra
 fi
-cd /Ajax
+cd /sra
 pip3 install -U -r requirements.txt
 echo "Starting ᗩᒍᗩ᙭....🔥"
 python3 bot.py
